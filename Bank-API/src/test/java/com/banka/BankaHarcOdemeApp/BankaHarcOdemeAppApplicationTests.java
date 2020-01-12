826c3b1d2bd7088126d0faa8f53d8f56c644ff34
ilyas ağlar <ilyas_693@hotmail.com>
2020-01-13 01:58:48 +0300

    created bank app

---
+++ Bank-API/src/test/java/com/banka/BankaHarcOdemeApp/BankaHarcOdemeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banka.BankaHarcOdemeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaHarcOdemeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
